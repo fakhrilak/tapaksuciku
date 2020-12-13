e786c8d6fb3fb37d8952bcec7b62acb36eb4ca9c
unknown <fakhrilak23@gmail.com>
2020-12-13 21:32:02 +0700

    Updates

---
+++ precache-manifest.e105ae82ccfb632469cc9b74c30dc2b5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2344d3a7bd25dda150ec37117632f5a5",
+    "url": "/tapaksuciku/index.html"
+  },
+  {
+    "revision": "9394620d371914897c67",
+    "url": "/tapaksuciku/static/css/2.751a9f5e.chunk.css"
+  },
+  {
+    "revision": "dfa7848c68e4aaab6684",
+    "url": "/tapaksuciku/static/css/main.7874c5b0.chunk.css"
+  },
+  {
+    "revision": "9394620d371914897c67",
+    "url": "/tapaksuciku/static/js/2.b8b1bd57.chunk.js"
+  },
+  {
+    "revision": "8301ce1f239e1b1a0f4ccdbe49e4ca13",
+    "url": "/tapaksuciku/static/js/2.b8b1bd57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa7848c68e4aaab6684",
+    "url": "/tapaksuciku/static/js/main.124f9f66.chunk.js"
+  },
+  {
+    "revision": "3aa3c7bca7fb5d1357fb",
+    "url": "/tapaksuciku/static/js/runtime-main.88940b9b.js"
+  },
+  {
+    "revision": "e44543eb36cbbe9f704e97eb6ca3acb2",
+    "url": "/tapaksuciku/static/media/logo.e44543eb.png"
+  },
+  {
+    "revision": "a517d07445ac57a405afab4b30350b1c",
+    "url": "/tapaksuciku/static/media/logo1.a517d074.png"
+  }
+]);
